50878584263779b02c2353772224751ef1727912
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2017-01-05 22:30:00 +0000

    Show on-line and off-line channels

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ var $streamResults = $('#stream-results');
 
 var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
-function generateURL(channel) {
-  return 'https://wind-bow.gomix.me/twitch-api/streams/' + channel + '?callback=?';
+function generateURL(type, channel) {
+  return 'https://wind-bow.gomix.me/twitch-api/' + type + '/' + channel + '?callback=?';
 }
 
-function generateResult(data) {
+function generateResult(channelData, streamData) {
   var $row = $('<li class="row result-row" />');
 
   var $text = $('<div class="col-sm-10" />');
-  var $channel = $('<h1 />', {channel: data.stream.channel.display_name});
-  var $game = $('<p />', {game: data.stream.game});
-  var $status = $('<p />', {status: data.stream.channel.status});
-  $text.append($channel);
+
+  var $displayName = $('<h2 />', {text: channelData.display_name});
+  var $game = $('<p />', {text: channelData.game});
+  var $status = $('<p />', {text: channelData.status});
+
+  $text.append($displayName);
   $text.append($game);
   $text.append($status);
   $row.append($text);
@@ -21,7 +23,7 @@ function generateResult(data) {
   var $logo = $('<div class="col-sm-2" />');
   var $img = $('<img />', {
     class: 'data-logo',
-    src: data.stream.channel.logo
+    src: channelData.logo
   });
   $logo.append($img);
   $row.append($logo);
@@ -31,12 +33,16 @@ function generateResult(data) {
 
 function getData(channel) {
   channels.forEach(function(channel) {
-    $.getJSON(generateURL(channel)).then(function(data) {
-
-      var game = data.stream.game;
-      Object.keys(game).forEach(function(key) {
-        $streamResults.append(generateResult(game[key]));
-      });
+    $.getJSON(generateURL('streams', channel)).then(function(streamData) {
+      if (streamData.stream != null) {
+        // If we have stream data, we also have channel data, so no need to request it.
+        $streamResults.append(generateResult(streamData.stream.channel, streamData));
+      } else {
+        // If we don't have stream data, we have to request channel data.
+        $.getJSON(generateURL('channels', channel)).then(function(channelData) {
+          $streamResults.append(generateResult(channelData, streamData));
+        });
+      }
     });
   });
 }
